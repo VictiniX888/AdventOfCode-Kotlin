3d28b8ce4f0fecc113f3d11f5d2628cb04d1c8f7
VictiniX888 <victiniy@gmail.com>
2020-12-20 10:17:14 +0800

    Remove hardcoded when branches for rule 8 and 11 in 2020 Day 19 Part 2
    The checkMatch function is now generalized to work for any combination of rules.

--- src/aoc2020/day19/Part2.kt
+++ src/aoc2020/day19/Part2.kt
@@ -2,7 +2,6 @@ package aoc2020.day19
 
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.util.*
 
 // The way I initially solved this was by editing the rules map as such:
 //      rules[8] = "42 +"
@@ -19,7 +18,10 @@ fun main() {
         s.split(": ").let { (a, b) ->
             a.toInt() to (b.removeSurrounding("\"").split(" | ").map { it.split(' ') })
         }
-    }
+    }.toMutableMap()
+
+    rules[8] = listOf(listOf("42"), listOf("42", "8"))
+    rules[11] = listOf(listOf("42", "31"), listOf("42", "11", "31"))
 
     val answer = countValidMessages(messages, rules)
     println(answer)
@@ -32,56 +34,22 @@ private fun countValidMessages(messages: List<String>, rules: Map<Int, List<List
 private fun String.checkMatch(rules: Map<Int, List<List<String>>>, currentRule: Int = 0): Set<String> {
     val validStrings = mutableSetOf<String>()
 
-    when (currentRule) {
-        8 -> {
-            val nextRule = 42
-            var remainingStrings = setOf(this)
-            while (remainingStrings.isNotEmpty()) {
-                remainingStrings = remainingStrings.flatMap { it.checkMatch(rules, nextRule) }.toSet()
-                validStrings += remainingStrings
-            }
-        }
-
-        11 -> {
-            val firstRule = 42
-            val secondRule = 31
-            var remainingStrings = setOf(this)
-            remainingStrings = remainingStrings.flatMap { it.checkMatch(rules, firstRule) }.toSet()
-
-            val stack: Stack<Set<String>> = Stack()
-            while (remainingStrings.isNotEmpty()) {
-                stack.push(remainingStrings)
-                remainingStrings = remainingStrings.flatMap { it.checkMatch(rules, firstRule) }.toSet()
-            }
-
-            while (stack.isNotEmpty()) {
-                remainingStrings = stack.pop()
-                repeat(stack.size + 1) { _ ->
-                    remainingStrings = remainingStrings.flatMap { it.checkMatch(rules, secondRule) }.toSet()
+    val nextRuleOptions = rules[currentRule] ?: error("Current rule $currentRule cannot be found")
+    for (nextRules in nextRuleOptions) {    // FLATMAP
+        var remainingStrings = setOf(this)
+        for (nextRule in nextRules) {   // ALL
+            if (nextRule.toIntOrNull() != null) {
+                remainingStrings = remainingStrings.flatMap { it.checkMatch(rules, nextRule.toInt()) }.toSet()
+            } else {    // assuming rules with characters are in the form { n: "c" }
+                if (this.isNotEmpty() && this.first() == nextRule.first()) {
+                    return setOf(this.drop(1))
+                } else {
+                    return setOf()
                 }
-                validStrings += remainingStrings
             }
         }
 
-        else -> {
-            val nextRuleOptions = rules[currentRule] ?: error("Current rule $currentRule cannot be found")
-            for (nextRules in nextRuleOptions) {    // FLATMAP
-                var remainingStrings = setOf(this)
-                for (nextRule in nextRules) {   // ALL
-                    if (nextRule.toIntOrNull() != null) {
-                        remainingStrings = remainingStrings.flatMap { it.checkMatch(rules, nextRule.toInt()) }.toSet()
-                    } else {    // assuming rules with characters are in the form { n: "c" }
-                        if (this.isNotEmpty() && this.first() == nextRule.first()) {
-                            return setOf(this.drop(1))
-                        } else {
-                            return setOf()
-                        }
-                    }
-                }
-
-                validStrings += remainingStrings
-            }
-        }
+        validStrings += remainingStrings
     }
 
     return validStrings
